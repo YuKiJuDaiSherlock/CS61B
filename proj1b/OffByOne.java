efbf112ab3c46a3e8a4e46ac36563b673fc4b472
Your Name <you@example.com>
2022-03-30 20:19:22 +0800

    3/30/4

--- proj1b/OffByOne.java
+++ proj1b/OffByOne.java
@@ -4,6 +4,9 @@ public class OffByOne implements CharacterComparator{
 
     @Override
     public boolean equalChars(char x, char y) {
+        if (x < 97 || x > 122 || y < 97 || y > 122) {
+            return false;
+        }
         if (x - y == 1 || x - y == -1) {
             return true;
         }
